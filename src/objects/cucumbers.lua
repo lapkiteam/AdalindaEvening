4d264b42b0c1d4e0d944c9dd6e0fda722d6d6a35
gretmn102 <erlgreywatson@gmail.com>
2025-04-20 20:10:55 +0300

    feat: improve the description of ingredients
    Closes: #6

--- src/objects/cucumbers.lua
+++ src/objects/cucumbers.lua
@@ -3,6 +3,15 @@ local ids = require "ids"
 obj {
   nam = ids.cucumbers.id,
   disp = "Огурцы",
+  dsc = function (this)
+    local where = this:where()
+    if where.nam == ids.fridge_inner then
+      return "В лотке для овощей спрятаны {огурцы}."
+    elseif where.nam == ids.bowl.id then
+      return false
+    end
+    return "На полу валяются {огурцы}."
+  end,
   tak = "Вооружаюсь боевыми огурцами.",
   inv = "Хорошие огурцы, крепкие. Была бы цепь, сделала бы из них нунчаки.",
   use = function (this, another)
--- src/objects/potato.lua
+++ src/objects/potato.lua
@@ -3,6 +3,13 @@ local ids = require "ids"
 obj {
   nam = ids.potato.id,
   disp = "Картошка",
+  dsc =function (this)
+    local where = this:where()
+    if where.nam == ids.bowl.id then
+      return false
+    end
+    return "На полу разлёгся {пакет с картошкой}."
+  end,
   tak = "Взваливаю на свои хрупкие плечи пакет с картошкой.",
   inv = "Картошка могла быть лучше, если бы сразу родилась в приготовленном виде.",
   use = function (this, another)
