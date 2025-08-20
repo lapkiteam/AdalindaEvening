38360b264e30702898c1ca28150c0e79386df611
gretmn102 <erlgreywatson@gmail.com>
2025-08-20 19:32:00 +0300

    feat: добавить разговор с бывшим, после которого он начинает трындеть по телефону

--- src/objects/ex_boyfriend.lua
+++ src/objects/ex_boyfriend.lua
@@ -1,6 +1,43 @@
 local ids = require "ids"
+local utils = require "utils"
+
+---@class ExBoyfriendModel
+---@field talked boolean
+local model = {}
+
+function model.new()
+  ---@type ExBoyfriendModel
+  local instance = {
+    talked = false,
+  }
+  return instance
+end
+
+---@param model ExBoyfriendModel
+function model.talk(model)
+  model.talked = true
+end
 
 obj {
   nam = ids.ex_boyfriend.id,
   disp = "Бывший",
+  state = model:new(),
+  dsc = function (this)
+    if this:inroom().nam == ids.corridor.id then
+      local state = this.state --[[@type ExBoyfriendModel]]
+      if state.talked then
+        return "{"..ids.ex_boyfriend.id.."|Бывший} трындит по телефону со своей новой пассией."
+      end
+      return "У порога топчется {"..ids.ex_boyfriend.id.."|бывший}."
+    end
+  end,
+  act = function (this)
+    local state = this.state --[[@type ExBoyfriendModel]]
+    if state.talked then
+      return "Я ему в прошлый раз всё высказала."
+    else
+      model.talk(state)
+      return "В ходе горячих объяснений выяснилось, что он пришел, видите ли, забрать свои огурцы, которые когда-то купил."
+    end
+  end
 }
--- src/rooms/corridor.lua
+++ src/rooms/corridor.lua
@@ -1,14 +1,8 @@
 local ids = require "ids"
-local utils = require "utils"
 
 room {
   nam = ids.corridor.id,
   disp = "Прихожая",
-  dsc = function (this)
-    if utils.has(this, ids.ex_boyfriend.id) then
-      pn("У порога топчется {"..ids.ex_boyfriend.id.."|бывший}.")
-    end
-  end,
   way = {
     path { "На кухню", ids.kitchen.id },
     path { "В зал", ids.hall.id },
